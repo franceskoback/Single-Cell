d7d61ed42b23331af0792d8e48d0ea4e0c429c1e
Frances Koback <franceskoback@dev2.wynton.ucsf.edu>
2021-09-23 10:31:37 -0700

    bash_script added

---
+++ bash_script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+module load CBI r
+Rscript Step1_Preprocessing.R /wynton/home/srivastava/franceskoback/Single-Cell/filtered_feature_bc_matrix /wynton/home/srivastava/franceskoback/Single-Cell/data/AggrSheet.csv /wynton/home/srivastava/franceskoback/Single-Cell/data/regev_lab_cell_cycle_genes.txt /wynton/home/srivastava/franceskoback/Single-Cell/data/noFilters_scoresAdded.rds
+echo Finished
